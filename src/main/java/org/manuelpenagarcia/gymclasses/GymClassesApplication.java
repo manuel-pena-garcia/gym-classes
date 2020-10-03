4cae77bca421d6a91e756db34ea75a049e7b11ed
ManuelPenaGarcia <manuel.pena25@gmail.com>
2020-10-03 09:48:09 +0200

    Add Basic Authorization

--- src/main/java/org/manuelpenagarcia/gymclasses/GymClassesApplication.java
+++ src/main/java/org/manuelpenagarcia/gymclasses/GymClassesApplication.java
@@ -2,12 +2,27 @@ package org.manuelpenagarcia.gymclasses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GymClassesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymClassesApplication.class, args);
+	}
+	
+	@Bean
+	ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties) {
+
+		return (evt) -> {
+
+			Integer port = serverProperties.getPort();
+			
+			System.out.println("Porto: " + port);
+		};
 	}
 
 }
